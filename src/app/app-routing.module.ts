3a582bbddddd53873bf8c051e19451f3b08223c1
rob grace <zwergie69@protonmail.com>
2023-02-24 10:59:40 +0100

    starting button logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "", component:HomeComponent
+},{
+  path:"About", component:AboutComponent
+},{
+  path:"Menu", component:MenuComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HomeHeroComponent } from './home-hero/home-hero.component';
+import { AboutHeroComponent } from './about-hero/about-hero.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    HomeHeroComponent,
+    AboutHeroComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
